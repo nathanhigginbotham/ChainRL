4a25da02bd12a3a85b69a7118ee1c7d02da6cca3
nathan <nathanhigginbotham@mail.com>
2023-02-08 16:21:20 +0000

    Added a2c Train

---
+++ orgym/imp1/ppo/test.py
@@ -0,0 +1,28 @@
+
+
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+import os
+cwd = os.getcwd()
+
+
+print("This file directory only")
+print(os.path.dirname(os.path.realpath(__file__)))
+
+
+from Users/nathan/Documents/GitHub/ChainRL/orgym/imp1/ import EvalCall
+
+
+# import pathlib
+# # pathlib.Path(__file__).parent.resolve()
+
+# pathlib.Path().resolve()
+
+# import os, sys
+# sys.path.insert(1, 'ChainRL/orgym/imp1')
+
+# print(sys.path)
+
+# from utils.study_model import EvalModel
