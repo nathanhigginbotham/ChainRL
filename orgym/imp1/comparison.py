c539837a82c715840f0d25532f166b3be4a7397c
callumfduffy <68689807+callumfduffy@users.noreply.github.com>
2023-02-09 11:13:15 +0000

    Add files via upload

---
+++ orgym/imp1/comparison.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+        
+def eval_comparison(ppo_eval, sac_eval, filepath=None):
+    '''
+    Probably should change into some kind of loop when
+    we want more algorithms
+    '''
+    fig, ax1 = plt.subplots(figsize=(15,8))
+    ax2 = ax1.twinx()
+    x = np.linspace(1, len(ppo_eval['mean_rewards']), len(ppo_eval['mean_rewards']))
+
+    ax1.fill_between(x, ppo_eval['cumlative_upper_rewards'], ppo_eval['cumlative_lower_rewards'], color='dodgerblue', alpha=0.4)
+    ax1.plot(x, ppo_eval['cumlative_rewards'], color='dodgerblue', label='PPO Cumlative Reward')
+    ax2.fill_between(x, ppo_eval['upper_rewards'], ppo_eval['lower_rewards'], color='orange', alpha=0.4)
+    ax2.plot(x, ppo_eval['mean_rewards'], color='orange', label='PPO Daily Reward')
+
+    ax1.fill_between(x, sac_eval['cumlative_upper_rewards'], sac_eval['cumlative_lower_rewards'], color='red', alpha=0.4)
+    ax1.plot(x, sac_eval['cumlative_rewards'], color='red', label='SAC Cumlative Reward')
+    ax2.fill_between(x, sac_eval['upper_rewards'], sac_eval['lower_rewards'], color='limegreen', alpha=0.4)
+    ax2.plot(x, sac_eval['mean_rewards'], color='limegreen', label='SAC Daily Reward')
+
+    ax1.set_title('Comparison of Cumlative and Daily Rewards')
+    ax1.set_ylabel('Cumlative reward')
+    ax2.set_ylabel('Daily day')
+    ax1.set_xlabel('Day')
+    ax1.spines['top'].set_visible(False)
+    ax2.spines['top'].set_visible(False)
+    ax1.legend()
+    ax2.legend()
+
+    if filepath != None:
+        plt.savefig(filepath)
+    else:
+        plt.show()
+
+
+
+def train_comparison(ppo_train, sac_train, filepath=None):
+    '''
+    train comparisons maybe needs to be turned into a loop too
+    '''
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.plot(ppo_train['episodes'][2::3], ppo_train['rewards'][2::3], '-', linewidth=0.5, color='dodgerblue')
+    ax.plot(sac_train['episodes'][2::3], sac_train['rewards'][2::3], '-', linewidth=0.5, color='orange')
+
+    ax.set_xlabel('Episode')
+    ax.set_ylabel('Reward')
+    ax.set_title('Reward of each episode as we train')
+    ax.spines[['top', 'right']].set_visible(False)
+    
+    if filepath != None:
+        plt.savefig(filepath)
+    else:
+        plt.show()
+
+
+def train_time(ppo_train, sac_train,):
+    '''
+    think I have computed times wrong
+    '''
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.plot(ppo_train['episodes'], ppo_train['time'], label='PPO')
+    ax.plot(sac_train['episodes'], sac_train['time'], label='SAC')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+
+
+    ppo_train = pd.read_csv('ppo/logs/ppo_train_results.csv')
+    ppo_eval = pd.read_csv('ppo/logs/ppo_eval_results.csv')
+    sac_train = pd.read_csv('sac/logs/sac_train_results.csv')
+    sac_eval = pd.read_csv('sac/logs/sac_eval_results.csv')
+
+    eval_comparison(ppo_eval, sac_eval, 'comparison_figures/eval_comparison.png') 
+    train_comparison(ppo_train, sac_train, 'comparison_figures/train_comparison.png')
+    train_time(ppo_train, sac_train)
