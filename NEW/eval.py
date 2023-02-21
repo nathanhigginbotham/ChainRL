cf6fd92dce7d83b5918a44ed9f9191ec3a92b1ad
callumfduffy <callum.1to1@gmail.com>
2023-02-21 13:31:38 +0000

    Perf: evaluation occurs in a class now
    Co-Authored-By: nathanhigginbotham <78663585+nathanhigginbotham@users.noreply.github.com>

--- NEW/eval.py
+++ NEW/eval.py
@@ -15,9 +15,9 @@ from stable_baselines3.common.env_util import make_vec_env
 from dictionary import algorithm_dictionary # This is a dictionary with the algorithms and their parameters
 from dictionary import environment_dictionary # This is a dictionary with the environments and their parameters
 
-from utils import GetFilePath # This is a function that creates a filepath based on the environment, model and hyperparameters passed as arguments
+from utils import get_filepath # This is a function that creates a filepath based on the environment, model and hyperparameters passed as arguments
 
-from utils import CreateEnv # This is a function that creates an environment based on an environment string passed as argument
+from utils import create_env # This is a function that creates an environment based on an environment string passed as argument
 
 
 
@@ -41,7 +41,7 @@ def parse_args():
 
 
 
-def GetBestModel(alg_string, filepath):
+def get_model(alg_string, filepath=None):
     """ Gets the best model from a given filepath
 
     Args:
@@ -72,127 +72,114 @@ Create and initialize all variables and containers.
 """
 
 
+class Evaluate:
+    def __init__(self, env, model, n_eval_episodes=10):
+        self.env = env
+        self.model = model
+        self.n_eval_episodes = n_eval_episodes
 
 
-def GetData(env, model, n_eval_episodes, filepath):
-    """Gets data from the model interacting with the environment
+    def run(self, filepath=''):
+        """Gets data from the model interacting with the environment
 
-    Args:
-        env (_type_): _description_
-        n_eval_episodes (_type_): _description_
-    """
-    obs = env.reset()
-    
-    # Here one may pull data listed above in the nomencalture section by adding the respective data name to the list below
-       
-    df_names = ['D', 'X', 'R', 'P', 'Y'] # Initialising DataFrame names
-    # ['Market Demand', 'Inventory Node Stock', 'Reorder Amount', 'Node Profit', 'Edge Quantities'] Translated from above
-
-    for episode in range(n_eval_episodes):
-        for timestep in range(env.num_periods):
-            action = model.predict(obs)
-            obs, reward, _, _ = env.step(action[0])
-
-        for df_name in df_names:
-            df = getattr(env, df_name) # Get the DataFrame from the environment for the specific data
-            
-            if not os.path.exists(filepath + f'eval/{episode}/'): # If the directory does not exist, create it
-                os.makedirs(filepath + f'eval/{episode}/')
-            
-            df.to_csv(filepath + f'eval/{episode}/{df_name}.csv') # Save the DataFrame as a csv file
-            
-        obs = env.reset() # Reset the environment for the next episode
-        env.seed_int = episode # Select a new seed for the next episode
+        Args:
+            env (_type_): _description_
+            n_eval_episodes (_type_): _description_
+        """
+        raw_rewards = np.zeros((self.n_eval_episodes, self.env.num_periods))
+        obs = self.env.reset()
         
+        # Here one may pull data listed above in the nomencalture section by adding the respective data name to the list below
         
-        
-def GetMeanReward(env, model, n_eval_episodes, filepath):
-    """Gets the mean reward from the model interacting with the environment
+        df_names = ['D', 'X', 'R', 'P', 'Y'] # Initialising DataFrame names
+        # ['Market Demand', 'Inventory Node Stock', 'Reorder Amount', 'Node Profit', 'Edge Quantities'] Translated from above
 
-    It saves this into an evaluations.csv file in the filepath directory
-    
-    This also includes other parameters such as: mean_rewards, std_rewards, upper_rewards, 
-                                                lower_rewards, cumrewards, lower_cumrewards, upper_cumrewards}
-
-
-    Args:
-        env (Gym Environment): Environment to be used
-        model (Stable Baselines Model): Model to be used
-        n_eval_episodes (_type_): Number of evaluation episodes
-        filepath (str): Path to the directory where the model is saved (determined by the environment, model and hyperparameters keys)
-    """
-    
-    obs = env.reset()
-    env_periods = env.num_periods
-    
-    raw_rewards = np.zeros((n_eval_episodes, env_periods))
-    x = np.linspace(1, env_periods, env_periods)
-
-    for i in range(n_eval_episodes):
-        for j in range(env_periods):
-            action = model.predict(obs)
-            obs, reward, _, _ = env.step(action[0])
-            raw_rewards[i,j] = reward 
-        obs = env.reset()
-
-    mean_rewards = np.mean(raw_rewards, axis=0)
-    std_rewards = np.std(raw_rewards, axis=0)
-    upper_rewards = mean_rewards + std_rewards
-    lower_rewards = mean_rewards - std_rewards
-
-    upper_cumrewards = []
-    lower_cumrewards = []
-    cumrewards = []
-
-    for i, v in enumerate(mean_rewards):
-        cumreward = np.sum(mean_rewards[:i])
-        cumrewards.append(cumreward)
-        upper_cumrewards.append(cumreward+np.sqrt(np.sum(std_rewards[:i])))
-        lower_cumrewards.append(cumreward-np.sqrt(np.sum(std_rewards[:i])))
-
-    cumrewards = np.array(cumrewards) 
-    upper_cumrewards = np.array(upper_cumrewards) 
-    lower_cumrewards = np.array(lower_cumrewards) 
-
+        for episode in range(self.n_eval_episodes):
+            for timestep in range(self.env.num_periods):
+                action = self.model.predict(obs)
+                obs, reward, _, _ = self.env.step(action[0])
+                raw_rewards[episode,timestep] = reward
+                
+            for df_name in df_names:
+                df = getattr(self.env, df_name) # Get the DataFrame from the environment for the specific data
+                
+                if not os.path.exists(filepath + f'/{episode}/'): # If the directory does not exist, create it
+                    os.makedirs(filepath + f'/{episode}/')
+                
+                df.to_csv(filepath + f'/{episode}/{df_name}.csv') # Save the DataFrame as a csv file
+                
+            obs = self.env.reset() # Reset the environment for the next episode
+            self.env.seed_int = episode # Select a new seed for the next episode
             
-    eval_results = {'mean_rewards': mean_rewards, 'std_rewards': std_rewards, 
-                'upper_rewards': upper_rewards, 'lower_rewards': lower_rewards,
-                'cumlative_rewards': cumrewards, 'cumlative_lower_rewards': lower_cumrewards,
-                'cumlative_upper_rewards': upper_cumrewards}       
+        return raw_rewards
+            
+    def mean_reward(self, rewards, filepath=None):
+        """Gets the mean reward from the model interacting with the environment
 
-    eval_df = pd.DataFrame(eval_results)
-    
-    savepath = filepath + 'eval/eval_results.csv'
-    
-    
-    eval_df.to_csv(savepath, index_label='step')
-    return
+        It saves this into an evaluations.csv file in the filepath directory
+        
+        This also includes other parameters such as: mean_rewards, std_rewards, upper_rewards, 
+                                                    lower_rewards, cumrewards, lower_cumrewards, upper_cumrewards}
 
 
+        Args:
+            env (Gym Environment): Environment to be used
+            model (Stable Baselines Model): Model to be used
+            n_eval_episodes (_type_): Number of evaluation episodes
+            filepath (str): Path to the directory where the model is saved (determined by the environment, model and hyperparameters keys)
+        """
+        
+        mean_rewards = np.mean(rewards, axis=0)
+        std_rewards = np.std(rewards, axis=0)
+        upper_rewards = mean_rewards + std_rewards
+        lower_rewards = mean_rewards - std_rewards
 
-def Evaluate(env_string=None, alg_string=None, hyper_string=None, **kwargs):
-    
-    filepath = GetFilePath(env_string, alg_string, hyper_string)
-    
-    model = GetBestModel(alg_string, filepath)
-    
-    env = CreateEnv(env_string)
-    
-    # Change 10 to change how many episodes to evaluate over
-    
-    GetData(env, model, 10, filepath) # Get data from the model interacting with the environment
-    GetMeanReward(env, model, 10, filepath) # Get mean reward for model
-    
-    return
+        upper_cumrewards = []
+        lower_cumrewards = []
+        cumrewards = []
 
+        for i, v in enumerate(mean_rewards):
+            cumreward = np.sum(mean_rewards[:i])
+            cumrewards.append(cumreward)
+            upper_cumrewards.append(cumreward+np.sqrt(np.sum(std_rewards[:i])))
+            lower_cumrewards.append(cumreward-np.sqrt(np.sum(std_rewards[:i])))
 
+        cumrewards = np.array(cumrewards) 
+        upper_cumrewards = np.array(upper_cumrewards) 
+        lower_cumrewards = np.array(lower_cumrewards) 
+
+                
+        eval_results = {'mean_rewards': mean_rewards, 'std_rewards': std_rewards, 
+                    'upper_rewards': upper_rewards, 'lower_rewards': lower_rewards,
+                    'cumlative_rewards': cumrewards, 'cumlative_lower_rewards': lower_cumrewards,
+                    'cumlative_upper_rewards': upper_cumrewards}       
+
+        eval_df = pd.DataFrame(eval_results)
+        
+        if filepath == None:
+            return eval_df
+        elif filepath == 'default':
+            savepath = filepath +'/eval_results.csv'
+            eval_df.to_csv(savepath, index_label='step')
+            return eval_df
+        elif filepath != None: #overides the default filepath
+            savepath = filepath+'/eval_results.csv'
+            eval_df.to_csv(savepath, index_label='step')
+            return eval_df
+        
 
 
 if __name__ == '__main__':
     #### Code here to be executed
     args = parse_args()
     
-    #Evaluate(env_string=args.env, alg_string=args.algo, hyper_string='default')
+    filepath = get_filepath(args.env, args.algo)    #, hyper_string=None)
 
-    print('0')
 
+    model = get_model(args.algo, filepath)
+    env = create_env(args.env)
+
+    eval_filepath = filepath + "eval/"
+    eval = Evaluate(env, model)
+    rewards = eval.run(filepath=eval_filepath)
+    eval.mean_reward(rewards, filepath=eval_filepath)
